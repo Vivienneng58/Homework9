3aae092bdc3012877da430f74a327de9dda5c82e
Vivienneng58 <79915214+Vivienneng58@users.noreply.github.com>
2022-05-05 16:05:24 +0800

    Add project files.

---
+++ homework9/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace homework9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_start = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Chinese = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.vocab_num = new System.Windows.Forms.NumericUpDown();
+            this.button_next = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.vocab_num)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入您想要背的单词数量：";
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(406, 32);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "开始";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Chinese);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.vocab_num);
+            this.panel1.Controls.Add(this.button_next);
+            this.panel1.Controls.Add(this.button_start);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(29, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(506, 293);
+            this.panel1.TabIndex = 3;
+            // 
+            // Chinese
+            // 
+            this.Chinese.AutoSize = true;
+            this.Chinese.Location = new System.Drawing.Point(19, 139);
+            this.Chinese.Name = "Chinese";
+            this.Chinese.Size = new System.Drawing.Size(0, 17);
+            this.Chinese.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "请输入以上中文对应的英文单词：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(41, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(22, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "第        个";
+            // 
+            // vocab_num
+            // 
+            this.vocab_num.Location = new System.Drawing.Point(229, 33);
+            this.vocab_num.Name = "vocab_num";
+            this.vocab_num.Size = new System.Drawing.Size(120, 22);
+            this.vocab_num.TabIndex = 10;
+            this.vocab_num.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // button_next
+            // 
+            this.button_next.Location = new System.Drawing.Point(406, 232);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(75, 23);
+            this.button_next.TabIndex = 9;
+            this.button_next.Text = "下一题";
+            this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.vocabulary);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.vocab_num)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Chinese;
+        private System.Windows.Forms.Button button_next;
+        private System.Windows.Forms.NumericUpDown vocab_num;
+    }
+}
+
---
+++ homework9/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Random;
+
+namespace homework9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int num;
+        public int index = 0;
+        public string word;
+        public string meaning;
+        public int correct = 0;
+
+        private void vocabularies(string voca_path)
+        {
+            StreamReader sr = new StreamReader(voca_path, Encoding.Default);
+            string str;
+            while ((str = sr.ReadLine()) != null)
+            {
+
+            }
+            sr.Close();
+        }
+
+        private void next_vocabulary(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = (index + 1).ToString();
+
+            Random rd = new Random();
+            int every = rd.Next(0, i);
+
+            string[] temp = new string[3];
+            temp = str1[every].Split(new Char[] { '\t', '\t', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            word = temp[0];
+            meaning = temp[1];
+
+            Chinese.Text = meaning;
+
+            index++;
+
+            button_next.Text = "跳过";
+
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            button_next.Enabled = true;
+            Chinese.Text = "";
+            num = (int)vocab_num.Value;
+            this.next_vocabulary(sender, e);
+        }
+
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            if (button_next.Text == "跳过")
+            {
+                textBox1.Text = word;
+                textBox1.Enabled = false;
+                if (index == num)
+                {
+                    MessageBox.Show("总共答对" + correct + "个单词");
+                    button_next.Enabled = false;
+                }
+                button_next.Text = "下一题";
+            }
+            else
+            {
+                textBox1.Enabled = true;
+                this.next_vocabulary(sender, e);
+            }
+        }
+
+        private void vocabulary(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (textBox1.Text == word)
+                {
+                    correct++;
+                    MessageBox.Show("正确");
+                    this.next_vocabulary(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("错误");
+                }
+            }
+        }
+
+        string[] str1 = new string[1409];
+        public int i = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_next.Enabled = false;
+            StreamReader sr = new StreamReader("Vocabulary.txt", Encoding.Default);
+            string str;
+            while ((str = sr.ReadLine()) != null)
+            {
+                str1[i] = str;
+                i++;
+            }
+
+            string[] temp = new string[3];
+            temp = str1[0].Split(new Char[] { '\t', '\t', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
